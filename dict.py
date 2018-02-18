5197d065d2c1fc51eef7f8ce6d4cee7dcf63efeb
Devang Chauhan <devngc@gmail.com>
2018-02-19 03:52:35 +0530

    get() method for the dictionaries

--- dict.py
+++ dict.py
@@ -23,3 +23,14 @@ print z
 # Sort dictionary based on value
 x = {"a": 1, "b": 2, "c": 3}
 print sorted(x.items(), key=lambda x: x[1])
+
+# Using get() method on dictionaries with its default arguments
+employees = {001: "Bob", 012: "Rob", "303": "John"}
+
+
+def greetings(id):
+    return "Hi {}!".format(employees.get(id, "there"))
+
+
+for key in employees.keys():
+    print greetings(key)
