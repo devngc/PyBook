82093cd84cf1c12fdb7d61613c2a8f0b2a3deb52
Devang Chauhan <devngc@gmail.com>
2018-02-23 21:20:19 +0530

    Private vs Public variables naming

--- privateVspublic.py
+++ privateVspublic.py
@@ -1,4 +1,15 @@
-# Following is an example of public attribute or variable.
-# It is intended to be used by the importer of
-# the module or by the user of the class.
-regular_lower_case = 0
+# Module names: all_lower_case
+# Globals and locals: all_lower_case
+# Functions and methods: all_lower_case
+# Class names and exception names: CamelCase
+# Constants: ALL_CAPS
+
+# Public attributes or variabels intended to be used by the
+# importer of this module or user of this class: regular_lower_case
+
+# Private attributes or variables fr internal use by the
+# module or class: _signle_leading_underscore
+
+# Private attributes that shouldn't be subclassed: __double_leading_underscore
+
+# Magic attributes: __double_underscores__
