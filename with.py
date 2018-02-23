71b6b5595fac44fbc0325b435202e01b3017aa00
Devang Chauhan <devngc@gmail.com>
2018-02-23 21:21:07 +0530

    Using with

---
+++ with.py
@@ -0,0 +1,26 @@
+with open("text.txt") as fh:
+    for line in fh:
+        line = line.rstrip()
+        print line
+# After the with block is executed, Python closes the file
+print "done!"
+print "\n"*2
+
+
+class MyClass(object):
+
+    def __enter__(self):
+        print "We have entered the 'with' block"
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print "We're leaving the 'with'"
+
+    def sayHi(self):
+        print "Hi, instance %s" % (id(self))
+
+
+with MyClass() as cc:
+    cc.sayHi()
+
+print "After the with block"
