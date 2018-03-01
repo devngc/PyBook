ed785f2844f6d90ffdda831937b8b95b67525cc4
Devang Chauhan <devngc@gmail.com>
2018-03-01 14:20:52 +0530

    privateVspublic variables update

--- privateVspublic.py
+++ privateVspublic.py
@@ -13,3 +13,41 @@
 # Private attributes that shouldn't be subclassed: __double_leading_underscore
 
 # Magic attributes: __double_underscores__
+
+
+class GetSet(object):
+
+    instance_count = 0
+
+    # Following is a special variable
+    # It is not intended to be subclassed
+    __mangled_name = "no privacy"
+
+    def __init__(self, value):
+        self._attrval = value
+        GetSet.instance_count += 1
+
+    @property
+    def var(self):
+        print "Getting the 'var' attribute"
+        return self._attrval
+
+    @var.setter
+    def var(self, value):
+        print "Setting the 'var' attribute"
+        self._attrval = value
+
+    @var.deleter
+    def var(self):
+        print "Deletig the 'var' attribute"
+        self._attrval = None
+
+
+cc = GetSet(5)
+cc.var = 10
+print cc._attrval
+# Comment out this line below and see what happens
+# Having double underscores in a variable name does infuse some privacy
+# print cc.__mangled_name
+# The mangled name attribute has to be called with this special way
+print cc._GetSet__mangled_name
