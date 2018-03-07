772b54a738ac34422e2f094aeeba6dbb8ca20d26
Devang Chauhan <devngc@gmail.com>
2018-03-07 13:14:17 +0530

    sorting dict based on values

--- dict.py
+++ dict.py
@@ -16,3 +16,7 @@ x = {"name": "devang", "age": 29}
 y = {"points": 25, "likes": ["science", "simulation"]}
 z = dict(x, **y)
 print z
+
+# Sort dictionary based on value
+x = {"a": 1, "b": 2, "c": 3}
+print sorted(x.items(), key=lambda x: x[1])
