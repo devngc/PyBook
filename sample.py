edbf6357287d5d86149b015dada509de9e2013d4
Devang Chauhan <devngc@gmail.com>
2018-03-10 22:53:11 +0530

    Testing the importance of comma in a list

--- sample.py
+++ sample.py
@@ -1,15 +1,7 @@
 """This file is frequently used to try new things. Contents change
 almost on a daily basis."""
 
-def threeMultis(lst):
-    if isinstance(lst, list):
-        resultList = [item for item in lst if item % 3 == 0 and item != 0]
-        return resultList
-    else:
-        return "List input of numbers required"
+a = ["Mostapha", "Chris"]
 
-
-print ("This is a really long string."
-       " Just trying this out right now."
-       " As per the latest book that I am reading"
-       " called 'Python-tricks'.")
+b = ["Mostapha",
+     "Chris",]
