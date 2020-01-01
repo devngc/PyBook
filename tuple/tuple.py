656ab38dae622e8899aebf25e9d86b12a42446bd
Devang <devngc@gmail.com>
2020-01-01 13:00:00 -0500

    Added tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,12 @@
+"""This script demonstrates the use of named tuple
+from the collection module"""
+
+from collections import namedtuple
+
+# Creating a class using the namedtuple method
+Car = namedtuple('Car', 'color mileage type')
+my_car = Car('red', 3812.4, "sedan")
+print my_car.color
+print my_car.mileage
+print my_car.type
+print my_car
