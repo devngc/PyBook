887699aa07064cb140868fc29ce0e390fd48fb03
Devang <devngc@gmail.com>
2020-12-23 00:05:25 -0500

    tuple module updated

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -6,7 +6,10 @@ from collections import namedtuple
 # Creating a class using the namedtuple method
 Car = namedtuple('Car', 'color mileage type')
 my_car = Car('red', 3812.4, "sedan")
-print my_car.color
-print my_car.mileage
-print my_car.type
-print my_car
+print(my_car.color)
+print(my_car.mileage)
+print(my_car.type)
+print(my_car)
+print(my_car.color)
+black_car = my_car._replace(color="Black")
+print(black_car)
