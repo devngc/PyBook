e3ae042a104857d4feab28431ed93c343319bce8
Devang <devngc@gmail.com>
2021-01-31 12:16:00 +0530

    Rename kwargs

---
+++ oop/03_kwargs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This module shows how key word arguments can be provided in the class
+constructor."""
+
+
+class Animal:
+    def __init__(self, **kwargs):
+        # The following are all class attributes
+        self.type = kwargs['type'] if 'type' in kwargs else 'Dog'
+        self.name = kwargs['name'] if 'name' in kwargs else 'Tommy'
+        self.sound = kwargs['sound'] if 'sound' in kwargs else 'Whow whow'
+
+    def get_type(self):
+        return self.type
+
+    def get_name(self):
+        return self.name
+
+    def get_sound(self):
+        return self.sound
+
+amir = Animal()
+print(amir.get_type())
