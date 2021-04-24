2acae1af0ee641734cd7797fc35ccbe8a8bb6781
Devang <devngc@gmail.com>
2021-04-24 22:10:22 +0530

    docs(conditional): add docstring

--- pybook/conditional/conditional.py
+++ pybook/conditional/conditional.py
@@ -1,5 +1,17 @@
 """This module shows the ternary operator"""
 
-hungry = False
-x = "Feed the bear" if hungry else "Do not feed the bear"
-print(x)
+
+def ternary_operator(val: bool) -> str:
+    """A function to show the use of ternary operator in Python.
+
+    Args:
+        val: A boolean
+
+    Returns:
+        A string
+    """
+    x = "Feed the bear" if val else "Do not feed the bear"
+    return x
+
+
+print(ternary_operator("dd"))
